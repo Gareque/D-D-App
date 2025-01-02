dfff122fd103de26164a04970f1ebbca4df6c35e
Gareque <grigeral@gmail.com>
2025-01-02 23:00:25 +0000

    updating the script functions

--- Index.ps1
+++ Index.ps1
@@ -23,17 +23,30 @@ function Get-Monster($monsterName, $baseUri) {
 
 # Function to print the spell details
 function Print-Spell($Spell) {
-        Write-Host "====== $($Spell.name) ======"
+        Write-Host "====== $($Spell.name) ====== `n"
     
+        Write-Host "Spell Details:" -ForegroundColor Green
+        Write-Host "School of Magic: $($Spell.school.name)"
         Write-Host "Spell Level: $($Spell.level)"
         Write-Host "Spell Name: $($Spell.name) `n"
-        Write-Host "Damage: $($Spell.damage.damage_at_slot_level) `n"
-        Write-Host "Damage: $($Spell.damage.damage_at_character_level) `n"
-        Write-Host "Save: $($Spell.dc.dc_type.name)"
+        
+        Write-Host "Spell Damage:" -ForegroundColor Green
+            if ($Spell.level -eq '0') {
+            Write-Host "Damage: $($Spell.damage.damage_at_character_level) `n" 
+        } else {
+            Write-Host "Damage: $($Spell.damage.damage_at_slot_level) `n"
+        }
+        Write-Host "Saves:" -ForegroundColor Green
+        Write-Host "Save Stat: $($Spell.dc.dc_type.name)"
+        if ($Spell.dc.dc_success -eq 'half') {
         Write-Host "Damage on Success: $($Spell.dc.dc_success) `n"
+        } else {
+            Write-Host "Damage on Success: N/A `n"
+        }
+
+        Write-Host "Spell range and shape:" -ForegroundColor Green
         Write-Host "Area of Effect: $($Spell.area_of_effect.size)"
         Write-Host "Spell shape: $($Spell.area_of_effect.type) `n"
-        Write-Host "School of Magic: $($Spell.school.name)"
     }
 
 # Function to print monster details
