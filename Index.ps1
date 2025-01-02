860e1d463c5b445b983146156b2162e7d45ba4c5
Gareque <grigeral@gmail.com>
2025-01-02 20:35:15 +0000

    Adding comments for the process

--- Index.ps1
+++ Index.ps1
@@ -5,13 +5,16 @@
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", "application/json")
 
+# Setting the category to be pulled from
 Write-Host "Please choose the category you want to look at?" -ForegroundColor Cyan
 $category = Read-Host
 
 Write-Host "You chose ${category}." -ForegroundColor Green
 
+# Setting the URI for the API call
 $uri = "https://www.dnd5eapi.co/api/${category}/"
 
+# Confirming the sub-category to call from
 Write-Host "Please confirm which ${category} you would like to know about" -ForegroundColor Cyan
 
 $userInput = Read-Host
