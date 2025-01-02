fa5473c333434ffbec6ac9616cef0b0c35da356e
Gareque <grigeral@gmail.com>
2025-01-02 20:18:41 +0000

    Initial script added

--- Index.ps1
+++ Index.ps1
@@ -1,5 +1,15 @@
-$uri = 'https://www.dnd5eapi.co/api/'
+# $uri = 'https://www.dnd5eapi.co/api/'
 
-$userCategory = Read-Host "What category would you like to call?"
+# $userCategory = Read-Host "What category would you like to call?"
 
-$
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Accept", "application/json")
+$category = Read-Host "What category would you like this to be about?"
+$uri = "https://www.dnd5eapi.co/api/${category}/"
+
+Write-Host "You chose ${category}."
+
+$userInput = Read-Host "Please confirm which ${category} you would like to know about"
+
+$response = Invoke-RestMethod $uri/$userInput -Method 'GET' -Headers $headers
+$response | ConvertTo-Json
